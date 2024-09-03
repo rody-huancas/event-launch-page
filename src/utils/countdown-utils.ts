a8db378617afe2900d1d72137f0b8352749672f7
Rody Huancas <rodyhuancas.04@gmail.com>
2024-09-03 09:56:51 -0500

    Feat: components

---
+++ src/utils/countdown-utils.ts
@@ -0,0 +1,14 @@
+export const calculateTimeToEvent = () => {
+  const eventDate = new Date();
+  const currentDate = new Date();
+  const timeRemaining = eventDate.getTime() - currentDate.getTime();
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+};
